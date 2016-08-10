0d183bbf26039b30759f8e69b52da11574dbcb70
erlebach <gordon.erlebach@gmail.com>
2016-08-10 16:37:30 -0400

    New class to handle inputs into the network.

---
+++ include/input.h
@@ -0,0 +1,29 @@
+#ifndef __INPUT_H__
+#define __INPUT_H__
+
+#include <vector>
+#include <string>
+#include "typedefs.h"
+
+// Class to help input data into the code. 
+// Read input from one of several file formats. The user can write Python 
+// code and write out the data in any one of several formats. 
+// The Input class will convert the data to VF2D format (nb_elements, dim). 
+// "dim" : dimensionality of the data (how many streams are input into the network 
+//
+
+class Input
+{
+private:
+	std::string name;
+	bool print_verbose;
+
+public:
+  Input();
+  ~Input();
+  Input(const Input&); 
+  const Input& operator=(const Input&); 
+  void print(std::string msg=std::string());
+};
+
+#endif
---
+++ src/input.cpp
@@ -0,0 +1,17 @@
+#include "input.h"
+
+Input::Input()
+{}
+
+Input::~Input()
+{}
+
+Input::Input(const Input& in)
+{}
+
+const Input& Input::operator=(const Input& in)
+{}
+
+void print(std::string msg=std::string())
+{
+}
