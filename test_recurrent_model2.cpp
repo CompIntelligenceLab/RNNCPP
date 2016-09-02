269bb94792b6a12a176e888712fc75f261e9ca38
erlebach <gordon.erlebach@gmail.com>
2016-09-02 15:54:14 -0400

    Prediction works.

--- test_recurrent_model2.cpp
+++ test_recurrent_model2.cpp
@@ -62,6 +62,17 @@ Input at t=0: x0
 Input at t=1: x1
 
 Loss = L = loss0 + loss1
+	z(0,0) = x0;
+	a(0,0) = z(0,0);
+	//a(1,-1) is retrieved via  layer->getConnection()->from->getOutputs();
+	z(1,0) = w01  * a(0,0); // + w11 * a(1,-1); // a(1,-1) is initially zero
+	a(1,0) = z(1,0);
+
+	z(0,1) = x1;
+	a(0,1) = z(0,1); // input <-- output  (set input to output)
+	z(1,1) = w01  * a(0,1) + w11 * a(1,0);   // for delay of 2, the 2nd term would be: w11 * a(1,-1)
+	a(1,1) = z(1,1);
+
 Forward: 
  a(1,-1) = 0, z(1,-1) = w11 * a(1,-1)
  a(2,-1) = 0, z(1,-1) = w22 * a(2,-1)
@@ -74,10 +85,14 @@ Forward:
  a(2,1) = z(2,1) = w2*z(1,1) + w12 * z(2,0)
 ***/
 
-	float w1       = .4;
-	float w12      = .5;
-	float w11      = .6;
-	float w22      = .7;
+	float w01 = .4;
+	float w12 = .5;
+	float w11 = .6;
+	float w22 = .7;
+	w01       = 1.;
+	w12       = 1.;
+	w11       = 1.;
+	w22       = 1.;
 	float x0       = .45;
 	float x1       = .75;
 	float ex0      = .75; // exact value
@@ -88,73 +103,83 @@ Forward:
 	VF2D a(nb_layers+1, seq_len); // assume al dimensions = 1
 	VF2D z(nb_layers+1, seq_len); // assume al dimensions = 1
 
-	z(0,0) = a(0,0) = x0;
-	z(0,1) = a(0,1) = x1;
-	a(1,0) = z(1,0) = w1  * z(0,0);
-	a(2,0) = z(2,0) = w12 * z(1,0);
+	// t=0
+	z(0,0) = x0;
+	a(0,0) = z(0,0);
+	//a(1,-1) is retrieved via  layer->getConnection()->from->getOutputs();
+	z(1,0) = w01  * a(0,0); // + w11 * a(1,-1); // a(1,-1) is initially zero
+	a(1,0) = z(1,0);
+	z(2,0) = w12 * a(1,0);
+	a(2,0) = z(2,0);
 
-
-	//a(1,0) = w1  * a(0,0);
-	//a(2,0) = w12 * a(1,0);
-
-	z(1,0) = w11 * a(1,0);
-	z(2,0) = w22 * a(2,0);
-	a(1,1) = z(1,1) = w1  * z(0,1) + z(1,0);
-	a(2,1) = z(2,1) = w12 * z(1,1) + z(2,0);
-
-	//a(1,1) = w1  * a(0,1) + w11 * a(1,0)
-	//a(2,1) = w12 * a(1,1) + w22 * a(2,0)
+	// t=1
+	z(0,1) = x1;
+	a(0,1) = z(0,1);
+	z(1,1) = w01 * a(0,1) + w11 * a(1,0); 
+	a(1,1) = z(1,1);
+	z(2,1) = w12 * a(1,1) + w22 * a(2,0);
+	a(2,1) = z(2,1);
 
 	// loss = loss0 + loss1
 	//      = (a(2,0)-ex0)^2 + (a(2,1)-ex1)^2
 	//      = (w12*a(1,0)-ex0)^2 + (w12*a(1,1)+w22*a(2,0)-ex1)^2
-	//      = (w12*w1*a(0,0)-ex0)^2 + (w12*(w1*a(0,1)+w11*a(1,0)) + w22*w12*w1*x0 -ex1)^2
-	//      = (w12*w1*x0-ex0)^2 + (w12*(w1*x1+w11*w1*x0) + w22*w12*w1*x0 -ex1)^2
+	//      = (w12*w01*a(0,0)-ex0)^2 + (w12*(w01*a(0,1)+w11*a(1,0)) + w22*w12*w01*x0 -ex1)^2
+	//      = (w12*w01*x0-ex0)^2 + (w12*(w01*x1+w11*w01*x0) + w22*w12*w01*x0 -ex1)^2
 	//      = (l1-ex0)^2 + (l2+l3-ex1)^2
 	//
 	// d(loss)/dw1  = 2*(l1-ex0)*w12*x0 + 2*(l2+l3-ex1)*(w12*x1) + 2*(l2+l3-ex1)*w22*w12*x0
-	// d(loss)/dw12 = 2*(l1-ex0)*w1*x0 + 2*(l2+l3-ex1)*(w1*x1+w11*w1*x0) + 2*(l2+l3-ex1)*w22*w1*x0
-	// d(loss)/dw11 = 2*(l2+l3-ex1)*(w12*w1*x0) 
-	// d(loss)/dw22 = 2*(l2+l3-ex1)*(w12*w1*x0) 
+	// d(loss)/dw12 = 2*(l1-ex0)*w01*x0 + 2*(l2+l3-ex1)*(w01*x1+w11*w01*x0) + 2*(l2+l3-ex1)*w22*w01*x0
+	// d(loss)/dw11 = 2*(l2+l3-ex1)*(w12*w01*x0) 
+	// d(loss)/dw22 = 2*(l2+l3-ex1)*(w12*w01*x0) 
 	//
+
+	float loss0 = (a(2,0)-ex0)*(a(2,0)-ex0);  // same as predict routine
+	float loss1 = (a(2,1)-ex1)*(a(2,1)-ex1);  // same as predict routine
 	float L0 = 2.*(a(2,0)-ex0);
 	float L1 = 2.*(a(2,1)-ex1);
 	float dldw1  = L0*w12*x0 + L1*(w12*x1+w12*w11*x0+w22*w12*x0); // CORRECT
-	float dldw12 = L0*w1*x0 + L1*(w1*x1+w11*w1*x0 + w22*w1*x0); // CORRECT
-	float dldw11 = L1*(w12*w1*x0); // CORRECT
-	float dldw22 = L1*(w12*w1*x0); // CORRECT
+	float dldw12 = L0*w01*x0 + L1*(w01*x1+w11*w01*x0 + w22*w01*x0); // CORRECT
+	float dldw11 = L1*(w12*w01*x0); // CORRECT
+	float dldw22 = L1*(w12*w01*x0); // CORRECT
 
-	//a11 = w1 *a01 + w11*a10;
+
+	printf("\n ============== Layer Outputs =======================\n");
+	printf("a00, a01= %f, %f\n", a(0,0), a(0,1));
+	printf("a10, a11= %f, %f\n", a(1,0), a(1,1));
+	printf("a20, a21= %f, %f\n", a(2,0), a(2,1));
+
+	//a11 = w01 *a01 + w11*a10;
 	//a21 = w12*a11 + w22*a20;
-	//a10 = w1*a00
-	//a20 = w1*w11*a10;
+	//a10 = w01*a00
+	//a20 = w01*w11*a10;
 
-	float da11da01 = w1;
+	float da11da01 = w01;
 	float da11da10 = w11;
 	float da21da11 = w12;
 	float da21da20 = w22;
-	float da10da00 = w1;
-	float da20da10 = w1*w11;
+	float da10da00 = w01;
+	float da20da10 = w01*w11;
 
 	printf("dLda20= %f\n", L0);
 	printf("dLda21= %f\n", L1);
-	printf("da11da01= %f\n", w1);
+	printf("da11da01= %f\n", w01);
 	printf("da11da10= %f\n", w11);
 	printf("da21da11= %f\n", w12);
 	printf("da21da20= %f\n", w22);
-	printf("da10da00= %f\n", w1);
-	printf("da20da10= %f\n", w1*w11);
+	printf("da10da00= %f\n", w01);
+	printf("da20da10= %f\n", w01*w11);
 	printf("\n\n");
 
 	float dLda20 = L0;
 	float dLda21 = L1;
 	float dLda11 = L1 * da21da11;
-	float dLda10 = L0*w1*w11;
-	float dLda01 = w1*w12*L1;
-	float dLda00 = w1*(L1*w11*w12 + L0*w1*w11);
-	//float dLda00 = da10da00*(L1*da11da10*da21d11 + L0*da20da10) = w1*(L1*w11*w12 + L0*w1*w11);
+	float dLda10 = L0*w01*w11;
+	float dLda01 = w01*w12*L1;
+	float dLda00 = w01*(L1*w11*w12 + L0*w01*w11);
+	//float dLda00 = da10da00*(L1*da11da10*da21d11 + L0*da20da10) = w01*(L1*w11*w12 + L0*w01*w11);
 
 	printf("Calculation of weight derivatives by hand\n");
+	printf("loss0= %f, loss1= %f\n", loss0, loss1);
 	printf("dldw1= %f\n", dldw1);
 	printf("dldw12= %f\n", dldw12);
 	printf("dldw11= %f\n", dldw11);
@@ -168,14 +193,6 @@ Forward:
 	printf("dLda10= %f\n", dLda10);
 	printf("dLda01= %f\n", dLda01);
 	printf("dLda00= %f\n", dLda00);
-
-
-	printf("a(1,1)= %f,  a(2,1)= %f\n", a(1,1), a(2,1));
-
-	float loss0 = (ex0-a(2,0))*(ex0-a(2,0));  // same as predict routine
-	float loss1 = (ex1-a(2,1))*(ex1-a(2,1));  // DIFFERENT FROM PREDICT ROUTINE
-	// Is error hand-solution or in predict? 
-	printf("loss0= %f, loss1= %f\n", loss0, loss1);
 
 	int output_dim = m->getOutputLayers()[0]->getOutputDim();
 
@@ -196,26 +213,30 @@ Forward:
 	Connection* conn;
 	{
 		conn = m->getConnection(input, d1);
-		WEIGHT& w1 = conn->getWeight();
-		w1(0,0) = 0.4;
+		WEIGHT& w_01 = conn->getWeight();
+		w_01(0,0) = w01;
+		conn->computeWeightTranspose();
 	}
 
 	{
 		conn = m->getConnection(d1, d2);
-		WEIGHT& w12 = conn->getWeight();
-		w12(0,0) = .5;
+		WEIGHT& w_12 = conn->getWeight();
+		w_12(0,0) = w12;
+		conn->computeWeightTranspose();
 	}
 	
 	{
 		conn = d1->getConnection();
-		WEIGHT& w11 = conn->getWeight();
-		w11(0,0) = .6;
+		WEIGHT& w_11 = conn->getWeight();
+		w_11(0,0) = w11;
+		conn->computeWeightTranspose();
 	}
 	
 	{
 		conn = d2->getConnection();
-		WEIGHT& w22 = conn->getWeight();
-		w22(0,0) = .7;
+		WEIGHT& w_22 = conn->getWeight();
+		w_22(0,0) = w22;
+		conn->computeWeightTranspose();
 	}
 
 
@@ -235,7 +256,9 @@ Forward:
 		U::print(xf, "xf");
 		pred = m->predictViaConnections(xf);
 		U::print(pred, "Prediction: pred");
+		pred.print("Prediction: pred");
 	}
+	exit(0);
 	Objective* obj = m->getObjective();
 	LOSS loss = (*obj)(exact, pred);
 	loss.print("loss");
