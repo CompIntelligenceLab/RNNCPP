Ran common::runtest(), which failed. FD derivatives are wrong.

stats.py
@@ -5,21 +5,24 @@ import matplotlib.pyplot as plt
 a = np.loadtxt("stats.out")
 sz = a.shape[1];
 
+nb = 1
+
 plt.subplot(2,2,1)
-for i in range(0,3):
+for i in range(0,nb):
 	plt.plot(a[:,i])
 plt.title("amplitude")
 
 plt.subplot(2,2,2)
-for i in range(3,6):
+for i in range(nb,2*nb):
 	plt.plot(a[:,i])
 plt.title("mean")
 
 plt.subplot(2,2,3)
-for i in range(6,9):
+for i in range(2*nb,3*nb):
 	plt.plot(a[:,i])
 plt.title("std dev")
 
-plt.show()
+plt.savefig("stats.pdf")
+#plt.show()
+#print "gordon"
 
-print a[1]