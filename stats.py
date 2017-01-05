dL/dW and dL/db work correctly (perhaps the sign is wrong) with gmm1d.
Results with sin(x) are not convincing. I have not yet used the generator to
create a signal via sampling of the gmm. Question: how long to run?

stats.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 a = np.loadtxt("stats.out")
 sz = a.shape[1];
 
-nb = 1
+nb = 3
 
 plt.subplot(2,2,1)
 for i in range(0,nb):